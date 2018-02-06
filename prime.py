df05e73f9e30ff876c64b74d0c0107d6708140b6
Zafrin Minoti <zafrin.minoti@gmail.com>
2018-02-06 14:10:21 -0500

    midpoint added to alg

--- prime.py
+++ prime.py
@@ -1,21 +1,20 @@
+from Math import sqrt
+
 def isPrime(num):
 	prime = False
+	midpoint = num//2
 
 	if num == 2:
 		prime = True
 	elif num > 2:
 		if num % 2 == 0:
 			prime = False
-	#first_primes = [3, 5, 7, 11]
-		else:
-			for n in range(3,num,2):
+			
+		elif num > 8:
+			for n in range(3,midpoint,2):
 				if num % n == 0:
 					prime = False
 					break
 	else:
 		prime = True
 	return prime
-
-print(isPrime(int(input('Number? '))))
-
-# 
