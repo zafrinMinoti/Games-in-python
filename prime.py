9f9c6ae9c6196f7388123a362e89d339cbbd7ca2
Zafrin Minoti <zafrin.minoti@gmail.com>
2018-02-06 22:36:21 -0500

    algorithm bug found and fixed. sqrt option

--- prime.py
+++ prime.py
@@ -1,20 +1,22 @@
-from Math import sqrt
+from Math import sqrt, ceil
 
 def isPrime(num):
-	prime = False
-	midpoint = num//2
+    prime = False
+    num_sqrt = ceil(sqrt(num))
 
-	if num == 2:
-		prime = True
-	elif num > 2:
-		if num % 2 == 0:
-			prime = False
-			
-		elif num > 8:
-			for n in range(3,midpoint,2):
-				if num % n == 0:
-					prime = False
-					break
-	else:
-		prime = True
-	return prime
+    if num == 2:
+        prime = True
+    elif num > 2:
+        if num % 2 == 0:
+            prime = False
+
+        elif num > 8:
+            for n in range(3,num_sqrt+1,2):
+                if num % n == 0:
+                    prime = False
+                    break
+                else:
+                    prime = True
+        else:
+            prime = True
+    return prime
